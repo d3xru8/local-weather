7d2a4583db8f317944671ad896fa09cf240ce764
Dexter Rualo <dexterrualo@gmail.com>
2017-01-13 21:10:58 +0800

    Updated index.html, js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,11 @@ $(document).ready(function() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       $.getJSON("https://api.apixu.com/v1/current.json?key=c88b615011b243ebabf162239171201&q="+position.coords.latitude+","+position.coords.longitude,function(json){
-        var data = JSON.parse(json);
-        $(".name").html(data.location.name);
-        $(".country").html(data.location.country);
-        $(".time").html(data.location.localtime);
-        $(".icon").attr("src","http:"+data.current.condition.icon);
-        $(".condition").html(JSON.stringify(data.current.condition.text));
-        $(".weather").html(JSON.stringify(data.current.condition.text));
+        $(".name").html(json.location.name);
+        $(".country").html(json.location.country);
+        $(".time").html(json.location.localtime);
+        $(".icon").attr("src",json.current.condition.icon);
+        $(".condition").html(JSON.stringify(json.current.condition.text));
       });
     });
   }
