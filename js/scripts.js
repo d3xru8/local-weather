39d1b462829236f9fe00426ce6e12d7d0f3eedde
Dexter Rualo <dexterrualo@gmail.com>
2017-01-15 00:09:30 +0800

    Weather condition icon and text alignment

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,6 @@ $(document).ready(function() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       $.getJSON("https://api.apixu.com/v1/current.json?key=c88b615011b243ebabf162239171201&q="+position.coords.latitude+","+position.coords.longitude,function(json){
-        console.log(json.current);
         temp_c = json.current.temp_c;
         temp_f = json.current.temp_f;
         $(".name").html(json.location.name);
