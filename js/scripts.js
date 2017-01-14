63e0e15cfdb5e254e77ed9e1c761d3b0fba8d0f8
Dexter Rualo <dexterrualo@gmail.com>
2017-01-15 00:33:17 +0800

    Round temperature to nearest whole number

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,8 @@ $(document).ready(function() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       $.getJSON("https://api.apixu.com/v1/current.json?key=c88b615011b243ebabf162239171201&q="+position.coords.latitude+","+position.coords.longitude,function(json){
-        temp_c = json.current.temp_c;
-        temp_f = json.current.temp_f;
+        temp_c = Math.round(json.current.temp_c);
+        temp_f = Math.round(json.current.temp_f);
         $(".name").html(json.location.name);
         $(".country").html(json.location.country);
         $(".time").html(json.location.localtime);
