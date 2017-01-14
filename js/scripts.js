c320cfa29b38afe62fbe8a4db8b5acb8d5e13dd6
Dexter Rualo <dexterrualo@gmail.com>
2017-01-14 15:41:49 +0800

    Started styling

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,15 @@ $(document).ready(function() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       $.getJSON("https://api.apixu.com/v1/current.json?key=c88b615011b243ebabf162239171201&q="+position.coords.latitude+","+position.coords.longitude,function(json){
-        var today = new Date(json.location.localtime_epoch);
+        console.log(json.current);
+        var temp_c = json.current.temp_c;
+        var temp_f = json.current.temp_f;
         $(".name").html(json.location.name);
         $(".country").html(json.location.country);
-        $(".time").html(today.toDateString());
+        $(".time").html(json.location.localtime);
         $(".icon").attr("src",json.current.condition.icon);
         $(".condition").html(json.current.condition.text);
-        $(".temp").html(JSON.stringify(json.location));
+        $(".temp").html(temp_c);
       });
     });
   }
